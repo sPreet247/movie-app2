2d844669f358055b26d3ec706f8d2eaaee3b9742
sPreet247 <samre2472@gmail.com>
2022-07-25 21:33:54 -0400

    path

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,43 @@
+import {
+  BrowserRouter as Router,
+  Navigate,
+  Route,
+  Routes,
+} from "react-router-dom";
 import "./app.scss";
+import Home from "./pages/home/Home.jsx";
 import Login from "./pages/login/Login.jsx";
+import Register from "./pages/register/Register.jsx";
+import Watch from "./pages/watch/Watch.jsx";
 
 const App = () => {
-  return <Login />;
+  const user = true;
+  return (
+    <Router>
+      <Routes>
+        <Route
+          exact
+          path="/"
+          element={user ? <Home /> : <Navigate to="/register" />}
+        />
+        <Route
+          path="/register"
+          element={!user ? <Register /> : <Navigate to="/" />}
+        />
+        <Route
+          path="/login"
+          element={!user ? <Login /> : <Navigate to="/" />}
+        />
+        {user && (
+          <>
+            <Route path="/movies" element={<Home type="movies" />} />
+            <Route path="/series" element={<Home type="series" />} />
+            <Route path="/watch" element={<Watch />} />
+          </>
+        )}
+      </Routes>
+    </Router>
+  );
 };
 
 export default App;
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,17 +1,37 @@
-import Featured from "../../components/Featured/Featured.jsx";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import Featured from "../../components/Featured/Featured";
 import List from "../../components/list/List";
 import Navbar from "../../components/navbar/Navbar";
 import "./home.scss";
 
-const Home = () => {
+const Home = ({ type }) => {
+  const [lists, setLists] = useState([]);
+  const [genre, setGenre] = useState(null);
+
+  useEffect(() => {
+    const getRandomLists = async () => {
+      try {
+        const res = await axios.get(
+          `lists ${type ? "?type=" + type : ""} & ${
+            genre ? "&genre=" + genre : ""
+          } `
+        );
+        setLists(res.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getRandomLists();
+  }, [type, genre]);
+
   return (
     <div className="home">
       <Navbar />
-      <Featured />
-      <List />
-      <List />
-      <List />
-      <List />
+      <Featured type={type} />
+      {lists.map((list) => (
+        <List list={list} />
+      ))}
     </div>
   );
 };
