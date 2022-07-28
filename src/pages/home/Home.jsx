55b38675b10cd711566bf1adce4c7f1ed7269cf9
sPreet247 <samre2472@gmail.com>
2022-07-27 21:18:21 -0400

    final client

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
-import Featured from "../../components/Featured/Featured";
+import Featured from "../../components/featured/Featured";
 import List from "../../components/list/List";
 import Navbar from "../../components/navbar/Navbar";
 import "./home.scss";
@@ -13,13 +13,20 @@ const Home = ({ type }) => {
     const getRandomLists = async () => {
       try {
         const res = await axios.get(
-          `lists ${type ? "?type=" + type : ""} & ${
+          `lists${type ? "?type=" + type : ""}${
             genre ? "&genre=" + genre : ""
-          } `
+          }`,
+          {
+            headers: {
+              token:
+                "Bearer " +
+                JSON.parse(localStorage.getItem("user")).accessToken,
+            },
+          }
         );
         setLists(res.data);
-      } catch (error) {
-        console.log(error);
+      } catch (err) {
+        console.log(err);
       }
     };
     getRandomLists();
@@ -28,7 +35,7 @@ const Home = ({ type }) => {
   return (
     <div className="home">
       <Navbar />
-      <Featured type={type} />
+      <Featured type={type} setGenre={setGenre} />
       {lists.map((list) => (
         <List list={list} />
       ))}
